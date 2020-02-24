1281d372a06bf7b087df34140a124c7db2045a54
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2020-02-23 17:55:47 -0800

    Rename fix to wrap

--- src/Data/Fixed.hs
+++ src/Data/Fixed.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DeriveFunctor #-}
@@ -24,21 +26,21 @@ fold is over any F-algebra f.
 -}
 class Functor f => Catamorphic f a | a -> f where
   -- | A fixed
-  unfix :: a -> f a
+  unwrap :: a -> f a
 
   -- | A catamorphism is a fold
   cata :: (f x -> x) -> a -> x
-  cata fx = fx . fmap (cata fx) . unfix
+  cata fx = fx . fmap (cata fx) . unwrap
   {-# INLINE cata #-}
 
 -- | An Anamorphism is a unfold, but this allows any F-algebra f.
 class Functor f => Anamorphic f a | a -> f where
   -- | A u
-  fix :: f a -> a
+  wrap :: f a -> a
 
   -- | A anamorphism is a unfold.
   ana  :: (x -> f x) -> x -> a
-  ana fx = fix . fmap (ana fx) . fx 
+  ana fx = wrap . fmap (ana fx) . fx 
   {-# INLINE ana #-}
 
 -- {- | We define the Fixed of an element as something that can 
@@ -52,19 +54,9 @@ class (Anamorphic f a, Catamorphic f a) => Fixed f a | a -> f where
 -- | If two itemes can be fixed to the same functor, then we can convert
 -- freely between them.
 hylo :: (Catamorphic f a, Anamorphic f b) => a -> b
-hylo = cata fix
+hylo = cata wrap
 {-# INLINE hylo #-}
 
--- | The standard definition of a fix data type.
-data Fix f = Fix { unFix :: f (Fix f) }
-  deriving (Generic)
-
-deriving instance Eq (f (Fix f)) => Eq (Fix f)
-deriving instance Ord (f (Fix f)) => Ord (Fix f)
-deriving instance Show (f (Fix f)) => Show (Fix f)
-
-instance Functor f => Catamorphic f (Fix f) where unfix = unFix
-instance Functor f => Anamorphic f (Fix f) where fix = Fix
 
 -- * Operations 
 -- We can derive a lot of operations from the catamorphism and anamorphism.
@@ -74,7 +66,7 @@ mapF ::
   (Catamorphic f a, Anamorphic g b) 
   => (f b -> g b) 
   -> a -> b
-mapF f = cata (fix . f)
+mapF f = cata (wrap . f)
 {-# INLINE mapF #-}
 
 -- | Map can be defined using a catamorphism and an anamorphism
@@ -109,14 +101,14 @@ traverseF ::
   (Applicative t, Catamorphic f a, Anamorphic g b) 
   => (f (t b) -> t (g b))
   -> a -> t b
-traverseF f = cata (fmap fix . f) 
+traverseF f = cata (fmap wrap . f) 
 {-# INLINE traverseF #-}
 
 traverseFx :: 
   (Bitraversable p, Applicative f, Catamorphic (p x) a, Anamorphic (p y) b) 
   => (x -> f y)
   -> a -> f b
-traverseFx f = cata (fmap fix . bitraverse f id) 
+traverseFx f = cata (fmap wrap . bitraverse f id) 
 {-# INLINE traverseFx #-}
 
 -- * Common F-Algebras
@@ -125,26 +117,39 @@ traverseFx f = cata (fmap fix . bitraverse f id)
 data MonoidF b a 
   = MAppend b a
   | MEmpty
-  deriving (Functor, Generic)
+  deriving (Functor, Foldable, Traversable, Generic)
 
 instance Bifunctor MonoidF where
   bimap f g = \case
     MAppend a b -> MAppend (f a) (g b)
     MEmpty -> MEmpty
-
 instance Bifoldable MonoidF where
   bifoldMap f g = \case
     MAppend a b -> (f a) <> (g b)
     MEmpty -> mempty
-
 instance Bitraversable MonoidF where
 
 instance Catamorphic (MonoidF b) [b] where
-  unfix = \case
+  unwrap = \case
     [] -> MEmpty
     a:rest -> MAppend a rest
-
 instance Anamorphic (MonoidF b) [b] where
-  fix = \case
+  wrap = \case
     MEmpty -> []
     MAppend a rest -> a:rest
+
+data ReadF b a 
+  = Read b a
+
+-- | The standard definition of a fix data type.
+data Fix f = Fix { unFix :: f (Fix f) }
+  deriving (Generic)
+
+deriving instance Eq (f (Fix f)) => Eq (Fix f)
+deriving instance Ord (f (Fix f)) => Ord (Fix f)
+deriving instance Show (f (Fix f)) => Show (Fix f)
+
+instance Functor f => Catamorphic f (Fix f) where unwrap = unFix
+instance Functor f => Anamorphic f (Fix f) where wrap = Fix
+
+
