55ebd64dff114dad13677ee11e67b32b3d6ed28f
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2020-03-30 10:47:33 +0200

    Improved nix-setup

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
-{ mkDerivation, base, hpack, stdenv }:
-mkDerivation {
-  pname = "fixedit";
-  version = "0.0.1";
-  src = ./.;
-  libraryHaskellDepends = [ base ];
-  libraryToolDepends = [ hpack ];
-  prePatch = "hpack";
-  homepage = "https://github.com/kalhauge/fixedit#readme";
-  description = "A fixed point conversion";
-  license = stdenv.lib.licenses.bsd3;
+{ pkgs ? import (builtins.fetchTarball "https://github.com/nixos/nixpkgs/archive/24c765c744b.tar.gz") {}
+, compiler ? "ghc882"
+}:
+let 
+  inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource; 
+  hpkgs = pkgs.haskell.packages."${compiler}";
+in 
+hpkgs.developPackage { 
+  root = gitignoreSource ./.;
+  name = "fixedit";
+  modifier = drv: pkgs.haskell.lib.addBuildTools drv (with hpkgs; [ cabal-install ghcid ]);
 }
