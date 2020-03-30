6523a3c52c605b6161af5b6a4f53c3a552cd5027
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2020-03-30 12:08:59 +0200

    Improve default.nix setup slightly

--- default.nix
+++ default.nix
@@ -1,12 +1,19 @@
 { pkgs ? import (builtins.fetchTarball "https://github.com/nixos/nixpkgs/archive/24c765c744b.tar.gz") {}
-, compiler ? "ghc882"
+, compiler ? "default"
 }:
 let 
   inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource; 
-  hpkgs = pkgs.haskell.packages."${compiler}";
+  hpkgs = 
+  (if compiler == "default" 
+  then pkgs.haskellPackages 
+  else pkgs.haskell.packages."${compiler}"
+  );
 in 
 hpkgs.developPackage { 
   root = gitignoreSource ./.;
   name = "fixedit";
-  modifier = drv: pkgs.haskell.lib.addBuildTools drv (with hpkgs; [ cabal-install ghcid ]);
+  modifier = drv: 
+  with pkgs.haskell.lib; 
+    addBuildTools drv (with hpkgs; [ cabal-install ghcid ])
+  ;
 }
